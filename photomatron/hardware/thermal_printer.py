52f27e4b2691c45919ef89c9c9771f7efc4893d2
MrFrangipane <contact@frangipane.me>
2024-04-08 17:11:47 +0200

    test new thermal printer

--- photomatron/hardware/thermal_printer.py
+++ photomatron/hardware/thermal_printer.py
@@ -1,18 +1,20 @@
 import os.path
-
-from PIL import Image
-from thermalprinter import ThermalPrinter
+import subprocess
 
 
 def print_(data, double_size=False):
-    with ThermalPrinter(port='/dev/serial0', heat_interval=40, most_heated_point=10) as printer:
-        if os.path.isfile(data):
-            printer.image(Image.open(data))
+    if os.path.isfile(data):
+        subprocess.check_call([
+            "lp", "-d", "ThermalPrinter", "-o", "fit-to-page", data
+        ])
 
-        else:
-            printer.out(data, double_height=double_size, double_width=double_size)
+    else:
+        subprocess.check_call([
+            "echo", f'"{data}"', "|", "lp", "-d", "ThermalPrinter"
+        ])
 
 
 def feed(line_count):
-    with ThermalPrinter(port='/dev/serial0') as printer:
-        printer.feed(line_count)
+    subprocess.check_call([
+        "echo", '""', "|", "lp", "-d", "ThermalPrinter"
+    ])
